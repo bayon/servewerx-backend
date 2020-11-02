78292108d2ebf8cdd8f7f8864b2521416387f23e
Bayon Forte <bayon@forteworks.com>
2020-11-02 09:50:09 -0500

    add example endpoint

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var app = express();
 app.listen(3003, () => {
  console.log("Server running on port 3003");
 });
+
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+   });

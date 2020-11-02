58242b41ac0c5720438bd2e95115e20a6224340d
Bayon Forte <bayon@forteworks.com>
2020-11-02 16:55:39 -0500

    changes to work for production.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.listen(3000, () => {
 });
 var IN_DEV = true;
 var MYSQL_PORT = 3306
-var MYSQL_PASSWORD = "servewerxAdm1n@@#"
+var MYSQL_PASSWORD = "password123"
 if(IN_DEV){
     MYSQL_PORT = 8889
     MYSQL_PASSWORD = 'root'
@@ -20,14 +20,14 @@ app.get("/url", (req, res, next) => {
     //var mysql = require('mysql')
     var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: 'api',
     password: MYSQL_PASSWORD,
-    database: 'serverwerx',
+    database: 'servewerx',
     port:MYSQL_PORT
     })
     var data 
     connection.connect()
-    connection.query('SELECT * FROM serverwerx.junk', function(err, rows, fields){
+    connection.query('SELECT * FROM servewerx.junk', function(err, rows, fields){
         if(err) throw err 
         console.log("rows:",rows);
         console.log("row[0]:",rows[0].name);

4bdab7ff0874fcfc45f0623aae20dd3c96562b46
Bayon Forte <bayon@forteworks.com>
2020-11-02 10:11:12 -0500

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
+//npm install cors
+var cors = require('cors')
 var app = express();
+app.use(cors()) 
 app.listen(3003, () => {
  console.log("Server running on port 3003");
 });
@@ -7,3 +10,19 @@ app.listen(3003, () => {
 app.get("/url", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
+
+
+
+
+
+   //install
+
+//use
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})

a7b2bce41ac4aced26ae4b09d276cf35def6adb0
Bayon Forte <bayon@forteworks.com>
2020-11-02 11:16:46 -0500

    change port number

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var express = require("express");
 var cors = require('cors');
 var app = express();
 app.use(cors());
-app.listen(3003, () => {
- console.log("Server running on port 3003");
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
 });
 
 app.get("/url", (req, res, next) => {

3fbbf500cd5e6ddd5bf756d14352d1e3fd15a85b
Bayon Forte <bayon@forteworks.com>
2020-11-02 14:48:15 -0500

    add code to connect to mysql database.

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ app.listen(3000, () => {
 });
 
 app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'serverwerxAdm1n@##',
+    database: 'serverwerx'
+    })
+
+    connection.connect()
+
+    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+    if (err) throw err
+
+    console.log('The solution is: ', rows[0].solution)
+    })
+
+   
+
+    res.json(["Tony","Lisa","Michael","Ginger","Food",rows[0].solution]);
+    
+    connection.end()
+
    });
 
 

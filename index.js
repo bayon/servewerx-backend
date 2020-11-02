4469e12d7750d651d0742c8690d94e3a61aab54c
Bayon Forte <bayon@forteworks.com>
2020-11-02 10:12:22 -0500

    remove cors dev code.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require("express");
 //npm install cors
-var cors = require('cors')
+var cors = require('cors');
 var app = express();
-app.use(cors()) 
+app.use(cors());
 app.listen(3003, () => {
  console.log("Server running on port 3003");
 });
@@ -14,15 +14,4 @@ app.get("/url", (req, res, next) => {
 
 
 
-
-   //install
-
-//use
-var express = require('express')
-var cors = require('cors')
-var app = express()
-
-app.use(cors())
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+ 

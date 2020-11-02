2c2115f59941ef8c9da185c5565f343d56dca402
Bayon Forte <bayon@forteworks.com>
2020-11-02 15:29:11 -0500

    connect api to mysql db

--- index.js
+++ index.js
@@ -10,29 +10,26 @@ app.listen(3000, () => {
 });
 
 app.get("/url", (req, res, next) => {
-    /*
+    
     //var mysql = require('mysql')
     var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'serverwerx'
+    database: 'serverwerx',
+    port:'8889'
     })
-
+    var data 
     connection.connect()
-
-    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-    if (err) throw err
-
-    console.log('The solution is: ', rows[0].solution)
-    var data = row[0].solution;
+    connection.query('SELECT * FROM serverwerx.junk', function(err, rows, fields){
+        if(err) throw err 
+        console.log("rows:",rows);
+        console.log("row[0]:",rows[0].name);
+        data = rows[0].name;
+        res.json(["Tony","Lisa","Michael","Ginger","Food",data]);
     })
-*/
-   
 
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-
-   //connection.end()
+   connection.end()
 
    });
 

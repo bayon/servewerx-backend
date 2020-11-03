6efe1c15e15637e5ea9d4bd698a75e38ac7cbf4a
Bayon Forte <bayon@forteworks.com>
2020-11-03 09:47:06 -0500

    set config defaults to production and make dev have to uncomment.

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 // config.js
 
+//Development: must uncomment for development
+
+  /*
+    process.env.NODE_ENV = "development"
+    process.env.HOST = "localhost"
+    process.env.PORT = 3000
+    process.env.DB_PORT = 8889
+    process.env.DB_PWD = "root"
+  */
  
-//Production: must uncomment 
-/*
-process.env.NODE_ENV = "production"
-process.env.HOST = "localhost"
-process.env.PORT = 3000
-process.env.DB_PORT = 3306
-process.env.DB_PWD = "password123"
-*/
   module.exports = {
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
     HOST: process.env.HOST || 'localhost',
     PORT: process.env.PORT || 3000,
-    DB_PORT: process.env.DB_PORT || 8889,
-    DB_PWD: process.env.DB_PWD || 'root'
+    DB_PORT: process.env.DB_PORT || 3306,
+    DB_PWD: process.env.DB_PWD || 'password123'
   }
